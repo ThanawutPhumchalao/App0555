70fcf823cd675ff0cfabaa0ab22ea07b7b51bb55
ThanawutPhumchalao <thanawutphumchalao@gmail.com>
2024-09-09 13:01:26 +0700

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Appo11',
+  webDir: 'www'
+};
+
+export default config;
